19f049eda5d2a890ed5eff85234088c06ff7df4f
brunomlopes <brunomlopes@gmail.com>
2010-09-24 22:50:24 +0100

    Added elmah as a dep

--- default.ps1
+++ default.ps1
@@ -10,4 +10,5 @@ Task Setup -depends FetchDependencies
 Task FetchDependencies {
     nu install fluentnhibernate --version 1.1.0.685
     nu install sparkviewengine --version 1.1.0.0
+    nu install elmah --version 1.1.11517.0.20100822
 }
